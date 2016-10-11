84fabca975958762168ad7569f2caad8daf2c906
Neo He <neohe2007@gmail.com>
2016-10-11 23:06:55 +0800

    init repo

---
+++ learn_libevent/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  learn_libevent
+//
+//  Created by heyong on 16/10/11.
+//  Copyright © 2016年 heyong. All rights reserved.
+//
+
+#include <iostream>
+
+#include "event2/event.h"
+
+int main() {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
